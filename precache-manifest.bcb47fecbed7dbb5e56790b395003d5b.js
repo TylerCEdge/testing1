38bfbea5b113d5a1e53b9c863f4f4ad0728af282
TylerCEdge <tylercedge@gmail.com>
2019-06-20 01:26:31 -0400

    Updates

---
+++ precache-manifest.bcb47fecbed7dbb5e56790b395003d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d944c63ce1e7a620ae0099cfba9a98",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "0da18f569f348dfbcbe5",
+    "url": "/testing/static/css/main.1cafac22.chunk.css"
+  },
+  {
+    "revision": "43012c7df9fe9cd1b811",
+    "url": "/testing/static/js/2.8cc58cbe.chunk.js"
+  },
+  {
+    "revision": "0da18f569f348dfbcbe5",
+    "url": "/testing/static/js/main.c90540d5.chunk.js"
+  },
+  {
+    "revision": "f1f30a4328be139affc6",
+    "url": "/testing/static/js/runtime~main.dfd23d7b.js"
+  }
+]);
